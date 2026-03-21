5445f19ff466bc92f188397c8ba24e7c36dad870
HamzaOubada2 <oubham220@gmail.com>
2026-03-21 20:05:07 +0000

    config database + main

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
-export class AppModule {}
+@Module({
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true
+        }),
+
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: (config: ConfigService) => ({
+                type: 'postgres',
+                host: config.get('DB_HOST'),
+                port: config.get('DB_PORT'),
+                username: config.get('DB_USERNAME'),
+                password: config.get('DB_PASSWORD'),
+                database: config.get('DB_NAME'),
+                entities: [__dirname + '/**/*.entity{.ts,.js}'],
+                synchronize: true
+            }),
+            inject: [ConfigService],
+        })
+    ]
+})
+export class AppModule { }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Global Validation
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+  }))
+
+  // CORS for react(frontend)
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  })
+
+  // Global prefix
+  app.setGlobalPrefix('api');
+
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`🚀 Server running on http://localhost:3000/api`);
+
 }
 bootstrap();
